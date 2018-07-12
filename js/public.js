e6fc89c96faa77ee2673fd26a9fc90a173f186f5
SaSaChu <teresa7397@gmail.com>
2018-07-12 12:30:52 +0800

    index 完成2/3 還有手機版

---
+++ js/public.js
@@ -0,0 +1,12 @@
+/**
+ * @author      ZEUS Design - http://www.zeusdesign.com.tw
+ * @copyright   Copyright (c) 2018 ZEUS Design
+ */
+ 
+$(function () {
+
+  $('.title_pic').imgLiquid ({
+    fill: false
+  });
+
+});
