2ada7dbc9524f8442930f423b74e373d00cb8797
SaSaChu <teresa7397@gmail.com>
2018-07-13 14:12:25 +0800

    首頁over

--- js/public.js
+++ js/public.js
@@ -5,14 +5,21 @@
  
 $(function () {
 
-  $('.title_pic').imgLiquid ({
-    fill: false
-  });
-  
-
   // 回到上面按鈕
   $('#top_btn').click (function () {
       $('html, body').animate ({ scrollTop: 0 }, 'slow');
   });
 
+
+  // 右邊menu
+   $('.m_list, #navbox .x,#cover').click(function() {
+     $('#navbox').toggleClass('sn');
+     $('#cover').toggleClass('show');
+   });
+
+
+  $('.title_pic').imgLiquid ({
+    fill: false
+  });
+  
 });
