510e50701738e447f8ad02283ad1f7c183898b38
SaSaChu <teresa7397@gmail.com>
2018-07-15 18:28:00 +0800

    Revert "Minify js、html, fix gh-pages path bug."
    This reverts commit 290d3179846c597dc335f2f4d0f2f26f8ccf0d2d.

--- js/public.js
+++ js/public.js
@@ -1,1 +1,165 @@
-function tt(){return(new Date).getTime()}$(function(){$("#top_btn").click(function(){$("html, body").animate({scrollTop:0},"slow")}),$(".m_list, #navbox .x,#cover").click(function(){$("#navbox").toggleClass("sn"),$("#cover").toggleClass("show")}),$(".title_pic").imgLiquid({fill:!1});var t={$e:$("#read_box"),$close:$("#read_box .close"),$pic:$("#read_box .pic"),$left:$("#read_box .left"),$right:$("#read_box .right"),$doubleLeft:$("#read_box .double-left"),$doubleRight:$("#read_box .double-right"),$bottomSpan:$("#read_box .bottom span"),now:-1,pics:[],inited:!1,addTime:!1,init:function(){return this.inited?this.inited:(this.$close.click(function(){this.close()}.bind(this)),this.$left.click(function(){this.prep()}.bind(this)),this.$right.click(function(){this.next()}.bind(this)),this.$doubleLeft.click(function(){this.now=0,this.pic()}.bind(this)),this.$doubleRight.click(function(){this.now=this.pics.length-1,this.pic()}.bind(this)),this.inited=!0,this.inited)},close:function(){this.$e.removeClass("s"),this.$pic.empty().addClass("h"),this.$bottomSpan.attr("data-a","0").attr("data-b","0"),this.pics=[],this.now=-1},show:function(t){this.init(),this.now=-1,this.pics=t,this.$bottomSpan.attr("data-a","0").attr("data-b","0"),this.$pic.empty().addClass("h"),this.$e.addClass("s"),this.next()},pic:function(){this.$bottomSpan.attr("data-a",this.now+1).attr("data-b",this.pics.length);var t=$("<img />").attr("src",this.pics[this.now]+(this.addTime?"?t="+tt():""));this.$pic.empty().addClass("h").append(t),t.load(function(){var i=t.get(0).width,s=t.get(0).height,e=$(window).width()-128-16,n=$(window).height()-55-16;i>e&&(s=e/i*s,i=e),s>n&&(i=n/s*i,s=n),this.$pic.css({top:"calc((100% - 55px - 4px * 2 - "+s+"px) / 2)",left:"calc((100% - "+i+"px) / 2)",width:i+"px",height:s+"px","line-height":s+"px"}).removeClass("h")}.bind(this))},prep:function(){this.now=--this.now<0?this.pics.length-1:this.now,this.pic()},next:function(){this.now=++this.now<this.pics.length?this.now:0,this.pic()}},i={$e:$("#video_box"),$title:$("#video_box .top span"),$video:$("#video_box .video"),$close:$("#video_box .close"),inited:!1,init:function(){return this.inited?this.inited:(this.$close.click(function(){this.close()}.bind(this)),this.inited=!0,this.inited)},show:function(t,i){this.init();var s=$("<iframe />").attr("src",i).attr("frameborder",0).attr("allow","autoplay; encrypted-media");this.$title.text(t),this.$video.append(s),this.$e.addClass("s")},close:function(){console.error("x"),this.$e.removeClass("s"),this.$video.empty(),this.$title.empty()}};$("*[data-video_box_title][data-video_box_src]").click(function(){i.show($(this).data("video_box_title"),$(this).data("video_box_src"))}),$(".banners").each(function(){var t=$(this),i=t.find(".items > *").length,s=t.data("type");t.find(".pic").imgLiquid();var e=$(Array.apply(null,{length:Math.ceil(i/s)}).map(function(){return $("<a />")})).map($.fn.toArray).click(function(){$(this).addClass("a").siblings().removeClass("a"),t.attr("data-i",$(this).index()*s+1)});t.find(".pages").empty().append($("<span />").append(e)),t.find(".left").click(function(){e.filter(".a").prev().length?e.filter(".a").prev().click():e.last().click()}),t.find(".right").click(function(){e.filter(".a").next().length?e.filter(".a").next().click():e.first().click()}),e.first().addClass("a")}),$(".read_box").click(function(){t.show($(this).data("pics"))})});
+/**
+ * @author      ZEUS Design - http://www.zeusdesign.com.tw
+ * @copyright   Copyright (c) 2018 ZEUS Design
+ */
+
+function tt () { return new Date ().getTime (); }
+ 
+$(function () {
+
+  // 回到上面按鈕
+  $('#top_btn').click (function () {
+      $('html, body').animate ({ scrollTop: 0 }, 'slow');
+  });
+
+
+  // 右邊menu
+   $('.m_list, #navbox .x,#cover').click(function() {
+     $('#navbox').toggleClass('sn');
+     $('#cover').toggleClass('show');
+   });
+
+
+  $('.title_pic').imgLiquid ({
+    fill: false
+  });
+
+
+  var read_box = {
+    $e: $('#read_box'),
+    $close: $('#read_box .close'),
+    $pic: $('#read_box .pic'),
+    $left: $('#read_box .left'),
+    $right: $('#read_box .right'),
+    $doubleLeft: $('#read_box .double-left'),
+    $doubleRight: $('#read_box .double-right'),
+    $bottomSpan: $('#read_box .bottom span'),
+    now: -1,
+    pics: [],
+    inited: false,
+    addTime: false,
+    init: function () {
+      if (this.inited)
+        return this.inited;
+
+      this.$close.click(function () { this.close(); }.bind(this));
+      this.$left.click(function () { this.prep(); }.bind(this));
+      this.$right.click(function () { this.next(); }.bind(this));
+      this.$doubleLeft.click(function () { this.now = 0; this.pic(); }.bind(this));
+      this.$doubleRight.click(function () { this.now = this.pics.length - 1; this.pic(); }.bind(this));
+
+      this.inited = true;
+
+      return this.inited;
+    },
+    close: function (pics) {
+      this.$e.removeClass ('s');
+      this.$pic.empty().addClass ('h');
+      this.$bottomSpan.attr('data-a', '0').attr('data-b', '0');
+      this.pics = [];
+      this.now = -1;
+    },
+    show: function (pics) {
+      this.init();
+
+      this.now = -1;
+      this.pics = pics;
+      this.$bottomSpan.attr('data-a', '0').attr('data-b', '0');
+      this.$pic.empty().addClass ('h');
+      this.$e.addClass ('s');
+
+      this.next ();
+    },
+    pic: function () {
+      this.$bottomSpan.attr('data-a', this.now + 1).attr('data-b', this.pics.length);
+      var $img = $('<img />').attr('src', this.pics[this.now] + (this.addTime ? '?t=' + tt () : ''));
+      this.$pic.empty().addClass('h').append($img);
+
+      $img.load (function () {
+        var w = $img.get (0).width, h = $img.get (0).height;
+        var mw = $(window).width() - 64 * 2 - 8 * 2, mh = $(window).height() - 55 - 8 * 2;
+
+        if (w > mw) { h = mw / w * h; w = mw; }
+        if (h > mh) { w = mh / h * w; h = mh; }
+
+        this.$pic.css({
+          'top': 'calc((100% - 55px - 4px * 2 - ' + h + 'px) / 2)',
+          'left': 'calc((100% - ' + w + 'px) / 2)',
+          'width': w + 'px',
+          'height': h + 'px',
+          'line-height': h + 'px',
+        }).removeClass('h');
+
+      }.bind(this));
+    },
+    prep: function () {
+      this.now = --this.now < 0 ? this.pics.length - 1 : this.now;
+      this.pic();
+    },
+    next: function () {
+      this.now = ++this.now < this.pics.length ? this.now : 0;
+      this.pic();
+    }
+  };
+  var video_box = {
+    $e: $('#video_box'),
+    $title: $('#video_box .top span'),
+    $video: $('#video_box .video'),
+    $close: $('#video_box .close'),
+    inited: false,
+
+    init: function () {
+      if (this.inited)
+        return this.inited;
+      this.$close.click(function () { this.close(); }.bind(this));
+      this.inited = true;
+      return this.inited;
+    },
+    show: function (title, src) {
+      this.init();
+      var $iframe = $('<iframe />').attr ('src', src).attr ('frameborder', 0).attr ('allow', 'autoplay; encrypted-media');
+      this.$title.text(title);
+      this.$video.append($iframe);
+      this.$e.addClass('s');
+    },
+    close: function () {
+      console.error ("x");
+      
+      this.$e.removeClass('s');
+      this.$video.empty();
+      this.$title.empty();
+    }
+  };
+
+  $('*[data-video_box_title][data-video_box_src]').click (function () {
+    video_box.show($(this).data('video_box_title'), $(this).data('video_box_src'));
+  });
+  $('.banners').each (function () {
+    var $that = $(this);
+
+    var total = $that.find('.items > *').length;
+    var type = $that.data('type');
+    $that.find('.pic').imgLiquid();
+
+    var $a = $(Array.apply(null, { length: Math.ceil(total / type) }).map (function () { return $('<a />'); })).map ($.fn.toArray).click(function () {
+      $(this).addClass('a').siblings().removeClass('a');
+      $that.attr('data-i', ($(this).index() * type + 1));
+    });
+
+    
+    $that.find('.pages').empty ().append($('<span />').append($a));
+    $that.find('.left').click (function () {
+      if ($a.filter('.a').prev().length) $a.filter('.a').prev().click();
+      else $a.last().click();
+    });
+    $that.find('.right').click (function () {
+      if ($a.filter('.a').next().length) $a.filter('.a').next().click();
+      else $a.first().click();
+    });
+    $a.first().addClass('a');
+  });
+  $('.read_box').click (function () {
+    read_box.show ($(this).data('pics'));
+  });
+  
+});
